180408d101c1938d3ec8cd36d81f690f3d947b1a
MYusufSARI <137108609+MYusufSARI@users.noreply.github.com>
2024-08-11 18:11:08 +0300

    Managed ResourceManager

--- Assets/BuilderDefender/Scripts/Managers/BuildingManager.cs
+++ Assets/BuilderDefender/Scripts/Managers/BuildingManager.cs
@@ -11,12 +11,16 @@ public class BuildingManager : MonoBehaviour
 
 
 
+    private void Awake()
+    {
+        buildingTypeList = (Resources.Load<BuildingTypeListSO>(typeof(BuildingTypeListSO).Name));
+        buildingType = buildingTypeList._list[0];
+    }
+
+
     private void Start()
     {
         mainCamera = Camera.main;
-
-        buildingTypeList = (Resources.Load<BuildingTypeListSO>(typeof(BuildingTypeListSO).Name));
-        buildingType = buildingTypeList._list[0];
     }
 
 
