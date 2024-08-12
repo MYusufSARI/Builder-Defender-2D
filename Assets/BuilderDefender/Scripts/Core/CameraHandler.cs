f57fc84e38e9da335e0fae4422822b9d6468dcac
MYusufSARI <137108609+MYusufSARI@users.noreply.github.com>
2024-08-12 20:16:13 +0300

    Update CameraHandler.cs

--- Assets/BuilderDefender/Scripts/Core/CameraHandler.cs
+++ Assets/BuilderDefender/Scripts/Core/CameraHandler.cs
@@ -1,17 +1,64 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class CameraHandler : MonoBehaviour
 {
+    [Header(" Elements ")]
+    [SerializeField] private CinemachineVirtualCamera cinemachineVirtualCamera;
+
+
+    [Header(" Data ")]
+    private float orthographicSize;
+    private float targetOrthographicSize;
+
+
+
+    private void Start()
+    {
+        orthographicSize = cinemachineVirtualCamera.m_Lens.OrthographicSize;
+
+        targetOrthographicSize = orthographicSize;
+    }
+
+
     private void Update()
+    {
+        HandleMovement();
+
+        HandleZoom();
+    }
+
+
+    private void HandleMovement()
     {
         float x = Input.GetAxisRaw("Horizontal");
         float y = Input.GetAxisRaw("Vertical");
 
         Vector3 moveDir = new Vector3(x, y).normalized;
-        float moveSpeed = 5f;
+        float moveSpeed = 30f;
 
         transform.position += moveDir * moveSpeed * Time.deltaTime;
+
+    }
+
+
+    private void HandleZoom()
+    {
+        float zoomAmount = 2f;
+
+        targetOrthographicSize += Input.mouseScrollDelta.y * zoomAmount;
+
+        float minOrthographicSize = 10f;
+        float maxOrthographicSize = 30f;
+
+        targetOrthographicSize = Mathf.Clamp(targetOrthographicSize, minOrthographicSize, maxOrthographicSize);
+
+        float zoomSpeed = 5f;
+
+        orthographicSize = Mathf.Lerp(orthographicSize, targetOrthographicSize, Time.deltaTime * zoomSpeed);
+
+        cinemachineVirtualCamera.m_Lens.OrthographicSize = orthographicSize;
     }
 }
