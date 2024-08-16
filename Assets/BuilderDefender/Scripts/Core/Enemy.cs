7a464f3f3310e85acde81103db73856c30d7e6df
MYusufSARI <137108609+MYusufSARI@users.noreply.github.com>
2024-08-16 12:32:12 +0300

    .

--- Assets/BuilderDefender/Scripts/Core/Enemy.cs
+++ Assets/BuilderDefender/Scripts/Core/Enemy.cs
@@ -20,17 +20,27 @@ public class Enemy : MonoBehaviour
     private Rigidbody2D rigidbody2D;
 
 
+    [Header(" Settings ")]
+    private float lookForTargetTimer;
+    private float lookForTargetTimerMax = 0.2f;
+
+
+
     private void Start()
     {
         rigidbody2D = GetComponent<Rigidbody2D>();
 
         targetTransform =  BuildingManager.Instance.GetHQBuilding().transform;
+
+        lookForTargetTimer = Random.Range(0f, lookForTargetTimerMax);
     }
 
 
     private void Update()
     {
         ManageMoving();
+
+        LookForTargetTimer();
     }
 
 
@@ -54,6 +64,50 @@ public class Enemy : MonoBehaviour
 
             healthManager.Damage(10);
             Destroy(gameObject);
+        }
+    }
+
+
+    private void LookForTargets()
+    {
+        float targetMaxRadius = 10f;
+
+        Collider2D[] collider2DArray= Physics2D.OverlapCircleAll(transform.position, targetMaxRadius);
+
+        foreach(Collider2D collider2D in collider2DArray)
+        {
+            Building building =  collider2D.GetComponent<Building>();
+
+            if (building != null)
+            {
+                // It's a building!
+                if (targetTransform == null)
+                {
+                    targetTransform = building.transform;
+                }
+                else
+                {
+                    if (Vector3.Distance(transform.position, building.transform.position)<
+                        Vector3.Distance(transform.position, targetTransform.position))
+                    {
+                        // Closer!
+                        targetTransform = building.transform;
+                    }
+                }
+            }
+        }
+    }
+
+
+    private void LookForTargetTimer()
+    {
+        lookForTargetTimer -= Time.deltaTime;
+
+        if (lookForTargetTimer < 0f)
+        {
+            lookForTargetTimer += lookForTargetTimerMax;
+
+            LookForTargets();
         }
     }
 }
