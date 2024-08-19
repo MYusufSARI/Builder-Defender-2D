2833330625f3c0dfd0aa8cb441a77737a8b41239
MYusufSARI <137108609+MYusufSARI@users.noreply.github.com>
2024-08-19 05:35:22 +0300

    .

--- Assets/BuilderDefender/Scripts/Core/Managers/EnemyWaveManager.cs
+++ Assets/BuilderDefender/Scripts/Core/Managers/EnemyWaveManager.cs
@@ -5,20 +5,49 @@ using UnityEngine;
 public class EnemyWaveManager : MonoBehaviour
 {
 
+    [Header(" Settings ")]
+    private float nextWaveSpawnTimer;
+    private float nextEnemySpawnTimer;
+    private int remainingEnemySpawnAmount;
+    private Vector3 spawnPosition;
+
 
     private void Start()
     {
-        SpawnWave();
+        nextWaveSpawnTimer = 3f;
+    }
+
+
+    private void Update()
+    {
+        nextWaveSpawnTimer -= Time.deltaTime;
+
+        if (nextWaveSpawnTimer < 0f)
+        {
+            SpawnWave();
+        }
+
+        if (remainingEnemySpawnAmount >0)
+        {
+            nextEnemySpawnTimer -= Time.deltaTime;
+
+            if (nextEnemySpawnTimer < 0f)
+            {
+                nextEnemySpawnTimer = Random.Range(0f, 0.2f);
+
+                Enemy.Create(spawnPosition + UtilsClass.GetRandomDirection() * Random.Range(0f, 10f));
+                remainingEnemySpawnAmount--;
+            }
+        }
     }
 
 
     private void SpawnWave()
     {
-        Vector3 spawnPosition = new Vector3(20, 0);
+        spawnPosition = new Vector3(40, 0);
 
-        for (int i = 0; i < 10; i++)
-        {
-            Enemy.Create(spawnPosition + UtilsClass.GetRandomDirection() * Random.Range(0f, 10f));
-        }
+        nextWaveSpawnTimer = 10f;
+
+        remainingEnemySpawnAmount = 10;
     }
 }
